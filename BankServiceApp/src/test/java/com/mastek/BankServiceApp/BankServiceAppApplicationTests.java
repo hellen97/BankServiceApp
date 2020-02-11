062e58dccc79d868db8e942ba971a01336894f2b
Hellen61291 <Hellen61291@GB-LD1LPW10G010.mastek.com>
2020-02-11 15:31:40 +0000

    Project Created

---
+++ BankServiceApp/src/test/java/com/mastek/BankServiceApp/BankServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastek.BankServiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
